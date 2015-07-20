727cacacb769bbf6b414fbca9a7ee31dba4231bd
vamakin <vamakin@gmail.com>
2015-07-20 12:27:05 +0300

    config description

--- config/config.php
+++ config/config.php
@@ -10,14 +10,11 @@ return [
     | URL naming form
     |--------------------------------------------------------------------------
     |
-    | Определяет, в каком числе будут представлены ресурсы в строке URL.
-    | К примеру, plural чтобы использовать URL вида 'test.com/api/posts'
-    | или singular чтобы использовать URL вида 'test.com/api/post'
+    | Specifies resources form in the url string
+    | For example, plural for URL like 'test.com/api/posts'
+    | or singular for URL like 'test.com/api/post'
     |
-    | Specifies in which form resources in the url string
-    | In example, 'test.com/api/posts' for plural form
-    | or 'test.com/api/post' for singular form
-    |
+    | Available Settings: singular, plural
     |
     */
 
@@ -28,13 +25,11 @@ return [
     | Table naming form
     |--------------------------------------------------------------------------
     |
-    | Определяет, в каком числе будут именоватся ресурсы в БД.
-    | К примеру, plural чтобы ресурсы именовались в множественном числе: posts, users и т.д.
-    | или singular чтобы ресурсы именовались в единственном числе: post, user и т.д.
+    | Specifies form of table in the database
+    | In example, 'posts' for plural form
+    | or 'post' for singular form
     |
-    | Specifies form of resources in the url string
-    | In example, 'test.com/api/posts' for plural form
-    | or 'test.com/api/post' for singular form
+    | Available Settings: singular, plural
     |
     */
 
@@ -49,9 +44,11 @@ return [
     | К примеру, plural чтобы связи ресурсов именовались в множественном числе: posts_id, users_id и т.д.
     | или singular чтобы связи ресурсов именовались в единственном числе: post_id, user_id и т.д.
     |
-    | Defines form of resources in the url string
-    | In example, 'test.com/api/posts' for plural form
-    | or 'test.com/api/post' for singular form
+    | Specifies form of relation fields in the database
+    | In example, 'posts_id' for plural form
+    | or 'post_id' for singular form
+    |
+    | Available Settings: singular, plural
     |
     */
 
